57f9375c693330f68e8178c7ea1251d87fd8a187
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-05 03:04:44 -0300

    Todos os requisitos menos o de mover

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const botaoCria = document.querySelector('#criar-tarefa');
 const botaoApaga = document.querySelector('#apaga-tudo');
 const botaoApagaCompletas = document.querySelector('#remover-finalizados');
 const botaoApagaSelecionada = document.querySelector('#remover-selecionado');
+const botaoSalva = document.querySelector('#salvar-tarefas');
 const ol = document.querySelector('#lista-tarefas');
 
 function removeSelected() {
@@ -41,6 +42,37 @@ botaoApagaCompletas.addEventListener('click', () => {
 });
 
 botaoApagaSelecionada.addEventListener('click', () => {
-  const selected = document.querySelector('.selected');
-  ol.removeChild(selected);
+  const selected = document.querySelectorAll('.selected');
+  for (let index = 0; index < selected.length; index += 1) {
+    ol.removeChild(selected[index]);
+  }
 });
+
+botaoSalva.addEventListener('click', () => {
+  const toDoList = {};
+  const li = document.querySelectorAll('li');
+  for (let index = 0; index < li.length; index += 1) {
+    toDoList[li[index].innerHTML] = li[index].className;
+  }
+  localStorage.setItem('toDoList', JSON.stringify(toDoList));
+});
+
+window.onload = () => {
+  if (localStorage.getItem('toDoList')) {
+    const toDoList2 = JSON.parse(localStorage.getItem('toDoList'));
+    const keys = Object.keys(toDoList2);
+    for (let index = 0; index < keys.length; index += 1) {
+      const createLi2 = document.createElement('li');
+      createLi2.innerHTML = keys[index];
+      createLi2.className = toDoList2[keys[index]];
+      createLi2.addEventListener('click', () => {
+        removeSelected();
+        createLi2.classList.add('selected');
+      });
+      createLi2.addEventListener('dblclick', () => {
+        createLi2.classList.toggle('completed');
+      });
+      ol.appendChild(createLi2);
+    }
+  }
+};
